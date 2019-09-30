19731d942d096bd9b4b55e8af6db9a7af4b44ba6
Luis M. González <49004851+lmgg777@users.noreply.github.com>
2019-09-30 16:08:33 -0500

    Update ApprovalCard.js

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -6,7 +6,7 @@ const ApprovalCard = props => {
       <div className="content">{props.children}</div>
       <div class="extra content">
         <div class="ui two buttons">
-          <div class="ui basic green button">Approve</div>
+          <div class="ui basic green button">Approved...</div>
           <div class="ui basic red button">Decline</div>
         </div>
       </div>
