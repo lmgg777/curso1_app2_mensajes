81a81d774be1258dd8c5c5b5ef71bdfa85afc44b
Luis M. González <49004851+lmgg777@users.noreply.github.com>
2019-10-14 17:35:02 -0500

    Add files via upload
    App update

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,27 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const CommentDetail = props => {
-  return(
+  return (
     <div className="comment">
-      <a href="/" className="avatar">
-        <img src={props.authorImage} alt="avatar"/>
+      <a href="/">
+        <img
+          src={props.image}
+          alt="avatar"
+          className="right floated mini ui image"
+        />
       </a>
-      
       <div className="content">
         <a href="/" className="author">
-          {props.author}
+          {props.autor}
         </a>
       </div>
-
-      <div className="metadata">
-        <span className="date">
-          {props.timeAgo}
-        </span>
+      <div className="metadata" style={{ margin: "7px 0" }}>
+        <span className="date">{props.fecha}</span>
       </div>
-
-      <div className="text">
-          {props.commentary}
-      </div>
+      <div className="description">{props.mensaje}</div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 import ApprovalCard from "./ApprovalCard";
 import CommentDetail from "./CommentDetail";
-import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
       <ApprovalCard>
         <CommentDetail
-          authorImage={faker.image.avatar()}
-          author={`${faker.name.firstName()} ${faker.name.lastName()}`}
-          timeAgo="Yesterday at 11:00AM"
-          commentary="React is awesome"
+          image={faker.image.avatar()}
+          autor={`${faker.name.firstName()} ${faker.name.lastName()}`}
+          fecha="Today at 5:00PM"
+          mensaje="Elliot requested permission to view your contact details"
         />
       </ApprovalCard>
-
       <ApprovalCard>
         <CommentDetail
-          authorImage={faker.image.avatar()}
-          author={`${faker.name.firstName()} ${faker.name.lastName()}`}
-          timeAgo="Today at 7:00PM"
-          commentary="I want to learn more about React"
+          image={faker.image.avatar()}
+          autor={`${faker.name.firstName()} ${faker.name.lastName()}`}
+          fecha="Yesterday at 11:00AM"
+          mensaje="Jenny wants to add you to the group best friends"
         />
       </ApprovalCard>
-
       <ApprovalCard>
         <CommentDetail
-          authorImage={faker.image.avatar()}
-          author={`${faker.name.firstName()} ${faker.name.lastName()}`}
-          timeAgo="Today at 1:00PM"
-          commentary="I like it very much!"
+          image={faker.image.avatar()}
+          autor={`${faker.name.firstName()} ${faker.name.lastName()}`}
+          fecha="Yesterday at 03:33AM"
+          mensaje="Adriana requested permission to view your contact details"
         />
       </ApprovalCard>
     </div>
